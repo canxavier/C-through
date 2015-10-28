0ebae8c03f762d9cb3b075c9be3ef507f540272a
can <canxavier@gmail.com>
2015-10-28 17:32:24 +0100

    schon besser aber noch nicht gut

--- supersajan3.c
+++ supersajan3.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 int main(){
-int anzahlcommands=1;
+int anzahlcommands=2;
 char *eingabearray=malloc(100);
 char **command=malloc((sizeof(char**))*anzahlcommands);
 char **c_through=malloc((sizeof(char**))*anzahlcommands);
@@ -12,7 +12,37 @@ char **c_through=malloc((sizeof(char**))*anzahlcommands);
 
 
 command[0]="printf";
-c_through[0]="\n\nprintf\n -------------------------------------------------------------\n|printf ist eine Funktion die einfach Killah ist.            |\n|Sie ist einfach hand zu haben und super cool!               |\n -------------------------------------------------------------\n";
+c_through[0]="\n\n\
+******************************************************************************\n\
+*printf                                                                      *\n\
+*                                                                            *\n\
+*Beispiel: printf(\"Hallo Welt\");                                             *\n\
+*                                                                            *\n\
+*                                                                            *\n\
+*                                                                            *\n\
+*                                                                            *\n\
+*                                                                            *\n\
+*                                                                            *\n\
+*                                                                            *\n\
+*                                                                            *\n\
+*                                                                            *\n\
+******************************************************************************\n";
+command[1]="scanf";
+c_through[1]="\n\n\
+******************************************************************************\n\
+*scanf                                                                       *\n\
+*                                                                            *\n\
+*Beispiel:                                                                   *\n\
+*int Ziel;                                                                   *\n\
+*    scanf(\"%d\",Ziel);                                                       *\n\
+*                                                                            *\n\
+*                                                                            *\n\
+*                                                                            *\n\
+*                                                                            *\n\
+*                                                                            *\n\
+*                                                                            *\n\
+*                                                                            *\n\
+******************************************************************************\n";
 scanf("%s",eingabearray);
 
 for(int i = 0;i<anzahlcommands;i++){
